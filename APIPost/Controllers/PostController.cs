cb9821d6d41f95e4354c8edb94c236147110eb8b
MatiasFGarciaDox <mfernandez8@estudiante.ceibal.edu.uy>
2024-08-26 18:54:20 -0300

    Funcinalidad basica para crear post lista

---
+++ APIPost/Controllers/PostController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Controlador;
+using System.Data;
+using APIPost.Models;
+
+namespace APIPost.Controllers
+{
+    public class PostController : ApiController
+    {
+        [Route("LinugaLink/post/crear/")]
+        public IHttpActionResult CrearPost(PostModel post)
+        {
+            ControlPosts.CrearPost(post.Contenido, post.Id_Cuenta.ToString());
+            Dictionary<string, string> resultado = new Dictionary<string, string>();
+            resultado.Add("mensaje", "post creado");
+            return Ok(resultado);
+        }
+    }
+}
---
+++ APIPost/Models/PostModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIPost.Models
+{
+    public class PostModel
+    {
+        public long Id_Post;
+        public string url_contenido;
+        public string tipo_contenido;
+        public string Contenido;
+        public int Id_Cuenta;
+    }
+}
