160f78aa6b63fd6611f019d413cd2356311a9c5d
MBorges156 <mborges1@estudiante.ceibal.edu.uy>
2024-09-30 19:42:28 -0300

    Comienzo de Autenticación

---
+++ APIAutenticar/Controllers/VerificaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using APIAutenticar.Models;
+
+namespace APIAutenticar.Controllers
+{
+    public class UsuarioController : VerificaController
+    {
+        [Route("ApiAut/login")]
+        public IHttpActionResult Login(UserModel usuario)
+        {
+            Dictionary<string, bool> resultado = new Dictionary<string, bool>();
+
+            bool autenticacion = UsuarioController.Login(usuario.nombre_usuario, usuario.contrasena);
+            resultado.Add("Resultado", autenticacion);
+
+            if (autenticacion)
+                return Ok(resultado);
+
+            return NotFound();
+        }
+    }
+}
---
+++ APIAutenticar/Models/AutenticarModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIAutenticar.Models
+{
+    public class AutenticarModel
+    {
+        public string nombre_usuario;
+        public string contrasena;
+    }
+}
